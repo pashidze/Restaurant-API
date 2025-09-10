e8d9ab6db19276762f6cf059c094ae1a11f13827
Pavel <pavlik.shevchenko@mail.ru>
2025-09-10 21:57:09 +0300

    Fix create and update order

--- app/Services/Order/OrderService.php
+++ app/Services/Order/OrderService.php
@@ -14,59 +14,13 @@ class OrderService
         try{
             DB::beginTransaction();
 
-            $day = date('d');
-            $month = date('m');
-
-            //поиск номера последнего заказа на текущий день
-            $last = Order::where('number', 'like', "Заказ-{$day}{$month}-%")->orderBy('number', 'desc')->first();
-
-            //Если заказы в текущий день уже были, то определяется следующий номер, в противном случае нумерация с 0001
-            if($last) {
-                $lastNumber = (int) substr($last->number, -4);
-                $nextNumber = str_pad($lastNumber + 1, 4, '0', STR_PAD_LEFT);
-            } else {
-                $nextNumber = '0001';
-            }
-            $number = "Заказ-{$day}{$month}-{$nextNumber}";
-            $data['number'] = $number;
-
-            //Получение списка блюд в заказе и их количества
-            /* Памятка: Может возникнуть ситуация, когда заказ будет иметь вид типа:
-            блюдо: хлеб, количество: 1
-            блюдо: хлеб, количество: 2
-            блюдо: хлеб, количество: 1
-            Это всё одна позиция, которую нужно объединить.
-            Цикл проходит по массиву блюд заказа и переносит в другой массив уникальные значения, повторные суммирует.*/
-            $listDish = $data['list_of_dishes'];
-            $merged = [];
-            foreach ($listDish as $item) {
-                $name = $item['name'];
-                $quantity = (int) $item['quantity'];
-                isset($merged[$name]) ? $merged[$name] += $quantity : $merged[$name] = $quantity;
-            }
-
-            //Подготовка данных для attach()
-            /*Памятка: так как изначально в коллекции только наименования блюд, то нужно достать их id.
-              Из БД выгружается коллекция блюд, присутствующих в заказе.
-              Далее в цикле для каждого блюда создаётся объект, позволяющий получить id блюда для связи в дальнейшем.*/
-            $dishes = Dish::whereIn('id', array_keys($merged))->get()->keyBy('id');
-            $attachData = [];
-            $cost = 0;
-            foreach ($merged as $name => $quantity) {
-                $dish = $dishes[$name] ?? null;
-                if ($dish) {
-                    $attachData[$dish->id] = ['quantity' => $quantity];
-                    $cost += $quantity * $dish->price;
-                }
-            }
-
-            unset($data['list_of_dishes']);
-            $data['number_of_items'] = count($merged);
+            $data['number'] = $this->findNumberOrder();
+            $data['number_of_items'] = 0;
             $data['date_of_creation'] = date('Y-m-d');
-            $data['cost'] = $cost;
+            $data['cost'] = 0.0;
+            $data['user_id'] = auth()->id();
 
             $order = Order::create($data);
-            $order->dishes()->attach($attachData);
 
             DB::commit();
 
@@ -84,45 +38,22 @@ class OrderService
         try{
             DB::beginTransaction();
 
-            //Если меняется список блюд, то нужно повторить процедуру как при создании
-            if(isset($data['list_of_dishes'])) {
-                $listDish = $data['list_of_dishes'];
-                $merged = [];
-                foreach ($listDish as $item) {
-                    $name = $item['name'];
-                    $quantity = (int) $item['quantity'];
-                    isset($merged[$name]) ? $merged[$name] += $quantity : $merged[$name] = $quantity;
-                }
+            $merged = $this->mergeData($data);
 
-                $dishes = Dish::whereIn('id', array_keys($merged))->get()->keyBy('id');
-                $attachData = [];
-                $cost = 0;
-                foreach ($merged as $name => $quantity) {
-                    $dish = $dishes[$name] ?? null;
-                    if ($dish) {
-                        $attachData[$dish->id] = ['quantity' => $quantity];
-                        $cost += $quantity * $dish->price;
-                    }
-                }
+            [$attachData, $cost] = $this->attachData($merged);
 
-                unset($data['list_of_dishes']);
-                $data['number_of_items'] = count($merged);
-                $data['cost'] = $cost;
+            unset($data['list_of_dishes']);
+            $data['number_of_items'] = count($merged);
+            $data['cost'] = $cost;
 
-                $order->update($data);
-                $order->dishes()->sync($attachData);
-
-            } else {
-                $order->update($data);
-            }
+            $order->update($data);
+            $order->dishes()->sync($attachData);
 
             DB::commit();
 
-            //Объект для JSON
             return $order;
         } catch (Exception $exception) {
             DB::rollBack();
-
             throw new Exception($exception->getMessage(), 500);
         }
     }
@@ -170,7 +101,7 @@ class OrderService
             $query = Order::query()->with(['dishes', 'user']);
 
             isset($data['number']) ? $query->where('number', 'like', '%' . $data['number'] . '%'): null;
-            isset($data['user_id']) ? $query->where('user_id', $data['user_id']): null;
+            isset($data['waiter_id']) ? $query->where('user_id', $data['waiter_id']): null;
             isset($data['cost']) ? $query->where('cost', $data['cost']): null;
 
             $page = $data['page'] ?? 1;
@@ -185,5 +116,64 @@ class OrderService
         } catch (Exception $exception) {
             throw new Exception($exception->getMessage(), 500);
         }
+    }
+
+    protected function findNumberOrder()
+    {
+        $day = date('d');
+        $month = date('m');
+
+        //поиск номера последнего заказа на текущий день
+        $last = Order::where('number', 'like', "Заказ-{$day}{$month}-%")->orderBy('number', 'desc')->first();
+
+        //Если заказы в текущий день уже были, то определяется следующий номер, в противном случае нумерация с 0001
+        if($last) {
+            $lastNumber = (int) substr($last->number, -4);
+            $nextNumber = str_pad($lastNumber + 1, 4, '0', STR_PAD_LEFT);
+        } else {
+            $nextNumber = '0001';
+        }
+
+        return "Заказ-{$day}{$month}-{$nextNumber}";
+    }
+
+    protected function mergeData($data)
+    {
+        //Получение списка блюд в заказе и их количества
+        /* Памятка: Может возникнуть ситуация, когда заказ будет иметь вид типа:
+        блюдо: хлеб, количество: 1
+        блюдо: хлеб, количество: 2
+        блюдо: хлеб, количество: 1
+        Это всё одна позиция, которую нужно объединить.
+        Цикл проходит по массиву блюд заказа и переносит в другой массив уникальные значения, повторные суммирует.*/
+        $listDish = $data['list_of_dishes'];
+        $merged = [];
+        foreach ($listDish as $item) {
+            $dish_id = $item['dish_id'];
+            $quantity = (int) $item['quantity'];
+            isset($merged[$dish_id]) ? $merged[$dish_id] += $quantity : $merged[$dish_id] = $quantity;
+        }
+
+        return $merged;
+    }
+
+    protected function attachData($merged)
+    {
+        //Подготовка данных для attach()
+        /*Памятка: так как изначально в коллекции только наименования блюд, то нужно достать их id.
+          Из БД выгружается коллекция блюд, присутствующих в заказе.
+          Далее в цикле для каждого блюда создаётся объект, позволяющий получить id блюда для связи в дальнейшем.*/
+        $dishes = Dish::whereIn('id', array_keys($merged))->get()->keyBy('id');
+        $attachData = [];
+        $cost = 0;
+        foreach ($merged as $dish_id => $quantity) {
+            $dish = $dishes[$dish_id] ?? null;
+            if ($dish) {
+                $attachData[$dish->id] = ['quantity' => $quantity];
+                $cost += $quantity * $dish->price;
+            }
+        }
+
+        return [$attachData, $cost];
     }
 }
